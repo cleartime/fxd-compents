4d7fb90658f25bdbd8104ce308fdbd715cc3f34a
gxx <562606139@qq.com>
2017-06-28 17:45:59 +0800

    6.28

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <alert :msg="msg"></alert>
+    <toask :msg='msg'></toask>
+    <fmask/>
+    <actionsheet></actionsheet>
   </div>
 </template>
 
 <style lang="scss">
+
 @import './lib/reset.css';
+@import 'lib/config';
 </style>
 
 <script>
-
+import actionsheet from './components/actionsheet/actionsheet.vue'
+import fmask from './components/mask/mask.vue'
 import alert from './components/alert/alert.vue'
+import toask from './components/toask/toask.vue'
 
   export default {
     name: 'app',
@@ -21,6 +28,9 @@ import alert from './components/alert/alert.vue'
     },
     components: {
       alert,
+      toask,
+      fmask,
+      actionsheet
     },
     mounted() {
     },
--- src/components/alert/alert.vue
+++ src/components/alert/alert.vue
@@ -1,66 +1,64 @@
 <template>
-  <div class="alert">
-    <p class='alert-msg'>{{msg}}</p>
-     <div class='alert-btn'>
-        <span @click='submit'>确定</span>
-        <span @click='cancel'>取消</span>
-     </div>
+  <div class="dialog alert">
+      <h2>{{msg}}</h2>
+      <p><span @click='cancel' class="cancel">取消</span><span class="submit" @click="submit">确认</span></p>
   </div>
 </template>
-
 <style lang="scss" scoped>
-$masterColor: #1da9ff !default;
-
-    .alert {
-      width:90%;
-      margin: 0 auto;
-      text-align: center;
-      height:2rem;
-      line-height:1rem;
-      font-size: .16rem;
-      border-radius: 5px;
-      border: 1px solid $masterColor;
-      .alert-msg {
-        font-size: .2rem;
-        color: red;
-        border-bottom: 1px solid $masterColor;
-      }
-      .alert-btn {
-        width:100%;
-        display:flex;
-        align-items:center;
-        justify-items:center;
-        span {
-          flex: 1;
-          
-        }
-        span:last-child {
-            border-left: 1px solid $masterColor;
-          }
-      }
+ .alert{
+    position: fixed;
+   left: 50%;
+   top:50%;
+   z-index: 101;
+    background: #fff;
+    border-radius: .3rem;
+    line-height: 22px;
+    height:2.1rem;
+     text-align: center;
+     max-width: 90%;
+     min-width: 3rem;
+   font-size: .28rem;
+   width: 4rem;
+   margin-left: -2rem;
+   background:#fff;
+   h2{
+     padding: .4rem .3rem;
+     font-weight: normal;
+     font-size: .28rem;
+     border-bottom: 1px solid #EBEBEB;
+   }
+   p{
+     position: relative;
+       padding: .4rem .3rem;
+     text-align: center;
+     display: flex;
+     justify-content: space-around;
+     &:after{
+       position: absolute;
+       left: 50%;
+       top: 0;
+       width: 1px;
+       height: 100%;
+       content: '';
+       background: #EBEBEB;
+     }
+   }
+    .submit{
+      color: #00aaee;
     }
-
+ }
 </style>
-
 <script>
-  export default {
-    name:'alert',
-    props: ['msg'],
-    data() {
-      return {
-      };
-    },
-    mounted() {
-    },
-    methods:{
-      submit(){
-        alert(1)
-      },
-      cancel(){
-        alert(2)
-      },
-      cb(){
-      }
+    export default{
+        name: 'alert',
+        props: ['msg'],
+        methods: {
+            cancel(){
+              this.$emit('alertCb',0);
+            },
+            submit(){
+              this.$emit('alertCb',1);
+            }
+        }
     }
-  };
 </script>
