d22d981b8fa3039488726be7b8c53fd6e18d7572
gxx <562606139@qq.com>
2017-07-04 17:12:24 +0800

    loading

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <!--<actionsheet></actionsheet>-->
     <!--<textInput :showIcon="true" :showCode="true" :iconUrl="icon" :placeholder='placeholder' :type="type" :model="item.message" @text_input_cb="val=>{item.message=val}"></textInput>-->
     <!--<textInput :showIcon="true" :showCode="true" :iconUrl="icon" :placeholder='placeholder' :type="type" :model="item.message2" @text_input_cb="val=>{item.message2=val}"></textInput>-->
-    <loading></loading>
+    <!--<loading></loading>-->
   </div>
 </template>
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 module.exports = {
 	alert : require('./components/common/alert/alert.vue'),
+    loading : require('./components/common/loading/loading.vue'),
     input : require('./components/ui/textInput/textInput.vue'),
+    radioTip : require('./components/ui/radioTip/radioTip.vue'),
 }
