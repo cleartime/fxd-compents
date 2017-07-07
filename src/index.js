3ee877c0dcdc350cf25c332e2c9c23ba7a81436c
gxx <562606139@qq.com>
2017-07-07 18:12:27 +0800

    mobileVerify

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ module.exports = {
     loading : require('./components/common/loading/loading.vue'),
     input : require('./components/ui/textInput/textInput.vue'),
     radioTip : require('./components/ui/radioTip/radioTip.vue'),
+    mobileVerify: require('./components/feature/mobileVerify/mobileVerify.vue'),
 }
